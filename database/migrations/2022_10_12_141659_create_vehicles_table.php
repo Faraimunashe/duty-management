3f67384c09b68269874719ad353cb307ee4f6ddd
Education 5.0 1 <faraimunashe.m11@gmail.com>
2022-10-12 16:43:51 +0200

    vehicles and category

---
+++ database/migrations/2022_10_12_141659_create_vehicles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id');
+            $table->string('make');
+            $table->string('model');
+            $table->string('engine_number');
+            $table->date('date_manufactured');
+            $table->integer('quantity');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
